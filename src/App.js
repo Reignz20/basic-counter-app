59115b559efd20268f95a28a735264121154ad6e
Roheel <roheelakhtar16@gmail.com>
2021-08-19 14:49:15 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+// import components
 
-function App() {
+
+
+
+const App = () => {  
+  
+
+    const [count, setCount] = useState(0);
+    const [counter, setCounters] = useState([]);
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+        <div className="top">
+          <h1>Counter</h1>
+        </div>
+      
+
+        <div className="container">
+            <button onClick={()=> {setCount(c => c + 1);}}>+</button>
+            <button onClick={()=> {setCount(c => c - 1);}}>-</button>
+            <button onClick={()=> {setCount(0)}}>RESET</button>
+            <button onClick={()=> setCounters([...counter, <App/>])}>Add new counter</button>
+            <div className="counter">{count}</div>            
+
+        </div>
+            
+          {counter}  
+        
+      </div>
+        
   );
 }
 
+
+
 export default App;
+ 
